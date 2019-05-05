a53aac6e8eec1896596f11d77f43f8b08b0c541d
Joel Lisboa <joelisboas@gmail.com>
2019-05-05 02:20:15 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -2,7 +2,16 @@ let lateral = document.querySelector(".nav-lat");
 let content = document.querySelectorAll(".content");
 let div_content = document.querySelectorAll(".div-content");
 let li= document.querySelectorAll("li");
+let botao= document.querySelectorAll(".botao_sanduiche")
 
+// ------------------------------------------
+
+// function mostrarMenu() {
+//     lateral.classList.toggle("show-nav");
+// }
+// botao.addEventListener("click", mostrarMenu);
+
+// ------------------------------------------------
 
 function mostrarConteudo(i){
     div_content[i].classList.toggle("show-content");
